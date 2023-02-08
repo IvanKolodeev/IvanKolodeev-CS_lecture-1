68e15dafa27bb9e41017dc21957380ac9d6b1883
Ivan K <ikolodeev002@gmail.com>
2023-02-08 20:14:06 +0200

    Sun

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
+Console.Write("Number 1 = ");
+Console.WriteLine(numberA);
+Console.Write("Number 2 = ");
+Console.WriteLine(numberB);
+Console.Write("SUM = ");
 Console.WriteLine(result);
